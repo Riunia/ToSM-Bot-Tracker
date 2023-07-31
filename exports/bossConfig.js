0ec38b634a560022bfaa2938f21cd80710b0611e
Jokoril <124014624+Jokoril@users.noreply.github.com>
2023-07-31 17:16:41 +0800

    Sorted bosses by area

--- exports/bossConfig.js
+++ exports/bossConfig.js
@@ -5,25 +5,31 @@ Then, recreate the interaction menu on the affected boss thread(s).
 */
 
 bossProperties = [
-    { name: 'Apophis',       value: 'apophis',       channels: 13,   key: 'apophisChannel' },
-    { name: 'Barbarossa',    value: 'barbarossa',    channels: 13,   key: 'barbarossaChannel' },
-    { name: 'Culton',        value: 'culton',        channels: 6,    key: 'cultonChannel' },
-    { name: 'Devourer',      value: 'devourer',      channels: 2,    key: 'devourerChannel' },
-    { name: 'Dragon',        value: 'dragon',        channels: 2,    key: 'dragonChannel' },
-    { name: 'Eva',           value: 'eva',           channels: 8,    key: 'evaChannel' },
-    { name: 'Frost Bot',     value: 'frostbot',      channels: 13,   key: 'frostbotChannel' },
-    { name: 'Haboela',       value: 'haboela',       channels: 15,   key: 'haboelaChannel' },
-    { name: 'Harrah',        value: 'harrah',        channels: 6,    key: 'harrahChannel' },
-    { name: 'Lucia',         value: 'lucia',         channels: 13,   key: 'luciaChannel' },
-    { name: 'Magma',         value: 'magma',         channels: 8,    key: 'magmaChannel' },
-    { name: 'Nakya',         value: 'nakya',         channels: 15,   key: 'nakyaChannel' },
-    { name: 'Robarg',        value: 'robarg',        channels: 13,   key: 'robargChannel' },
-    { name: 'Rudolph',       value: 'rudolph',       channels: 8,    key: 'rudolphChannel' },
-    { name: 'Scylla',        value: 'scylla',        channels: 15,   key: 'scyllaChannel' },
-    { name: 'Sobek',         value: 'sobek',         channels: 13,   key: 'sobekChannel' },
-    { name: 'Zhuyan',        value: 'zhuyan',        channels: 15,   key: 'zhuyanChannel' },
-    { name: 'Black Crow',    value: 'blackcrow',     channels: 15,   key: 'blackcrowChannel' },
-    { name: 'Taotie',        value: 'taotie',        channels: 15,   key: 'taotieChannel' },
+    //Aesperia
+    { name: 'Apophis',       value: 'apophis',       channels: 37,   key: 'apophisChannel' },
+    { name: 'Barbarossa',    value: 'barbarossa',    channels: 37,   key: 'barbarossaChannel' },
+    { name: 'Frost Bot',     value: 'frostbot',      channels: 37,   key: 'frostbotChannel' },
+    { name: 'Lucia',         value: 'lucia',         channels: 37,   key: 'luciaChannel' },
+    { name: 'Robarg',        value: 'robarg',        channels: 37,   key: 'robargChannel' },
+    { name: 'Sobek',         value: 'sobek',         channels: 37,   key: 'sobekChannel' },
+    //AI
+    { name: 'Devourer',      value: 'devourer',      channels: 6,    key: 'devourerChannel' },
+    { name: 'Dragon',        value: 'dragon',        channels: 6,    key: 'dragonChannel' },
+    //Vera
+    { name: 'Eva',           value: 'eva',           channels: 30,    key: 'evaChannel' },
+    { name: 'Magma',         value: 'magma',         channels: 30,    key: 'magmaChannel' },
+    { name: 'Rudolph',       value: 'rudolph',       channels: 30,    key: 'rudolphChannel' },
+    //Abyss
+    { name: 'Culton',        value: 'culton',        channels: 20,    key: 'cultonChannel' },
+    { name: 'Harrah',        value: 'harrah',        channels: 20,    key: 'harrahChannel' },
+    //Innars
+    { name: 'Haboela',       value: 'haboela',       channels: 30,   key: 'haboelaChannel' },
+    { name: 'Scylla',        value: 'scylla',        channels: 30,   key: 'scyllaChannel' },
+    { name: 'Nakya',         value: 'nakya',         channels: 30,   key: 'nakyaChannel' },
+    //Domain Nine
+    { name: 'Zhuyan',        value: 'zhuyan',        channels: 50,   key: 'zhuyanChannel' },
+    { name: 'Black Crow',    value: 'blackcrow',     channels: 50,   key: 'blackcrowChannel' },
+    { name: 'Taotie',        value: 'taotie',        channels: 50,   key: 'taotieChannel' },
     //{name: 'boss_name',     value: 'boss_name_lowercase',   channels: 0,  key: 'boss_nameChannel'},
 ];
 
